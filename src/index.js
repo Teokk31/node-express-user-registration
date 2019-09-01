75aaca7c4bbb3fe18436ad47b7a686ff107de087
Khai Kiong Teo <khaikiong.teo@viewpoint.com.my>
2019-09-01 12:20:01 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ const getUsers = (request, response) => {
             return {
                 id: user.id,
                 name: user.name,
-                email: user.password
+                email: user.email
             };
         });
 
